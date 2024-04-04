1c854d87a475b6e5c4ce77e74c30483fc4eb5cd7
kookhh0827 <kookhh0827@gmail.com>
2024-04-04 11:07:13 +0000

    fix: add generateStaticParams for restaurants

--- src/app/restaurant/[id]/page.tsx
+++ src/app/restaurant/[id]/page.tsx
@@ -1,6 +1,7 @@
 // @/app/restaurant/[id].tsx
 
 import { Tables } from '@/lib/database.types';
+import { createPrivateClient } from '@/lib/supabase/private';
 import { createClient } from '@/lib/supabase/server';
 
 import CommentBox from '@/components/CommentBox';
@@ -9,9 +10,25 @@ import MealCalendar from '@/components/MealCalendar';
 import OrderCalendar from '@/components/OrderCalander';
 import RestaurantTabs from '@/components/RestaurantTabs';
 
+export async function generateStaticParams() {
+  const supabase = createPrivateClient();
+  const { data: restaurants } = await supabase
+    .from('restaurants')
+    .select('*')
+    .order('restaurant_id');
+
+  return (
+    restaurants &&
+    restaurants.map((restaurant) => ({
+      id: restaurant.restaurant_id.toString(),
+    }))
+  );
+}
+
 type Restaurant = Tables<'restaurants'>;
 
-const OrderItemReviewPage = async ({ params }: { params: { id: number } }) => {
+const OrderItemReviewPage = async ({ params }: { params: { id: string } }) => {
+  const id = parseInt(params.id);
   const supabase = createClient();
   const { data: restaurants } = await supabase
     .from('restaurants')
@@ -23,16 +40,16 @@ const OrderItemReviewPage = async ({ params }: { params: { id: number } }) => {
       <Header></Header>
       <RestaurantTabs
         restaurants={restaurants as Restaurant[]}
-        selectedRestaurant={params.id}
+        selectedRestaurant={id}
       ></RestaurantTabs>
       {restaurants &&
-      restaurants.find((restaurant) => restaurant.restaurant_id == params.id)
-        ?.type === 'meal' ? (
-        <MealCalendar restaurantId={params.id} />
+      restaurants.find((restaurant) => restaurant.restaurant_id == id)?.type ===
+        'meal' ? (
+        <MealCalendar restaurantId={id} />
       ) : (
-        <OrderCalendar restaurantId={params.id} />
+        <OrderCalendar restaurantId={id} />
       )}
-      <CommentBox restaurant_id={params.id}></CommentBox>
+      <CommentBox restaurant_id={id}></CommentBox>
     </div>
   );
 };
