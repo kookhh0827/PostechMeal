33406d9f922fc9369f2e3f4fac6f4391d81d34c9
kookhh0827 <kookhh0827@gmail.com>
2024-04-05 15:12:16 +0000

    fix: change the restaurant tabs design

--- src/components/RestaurantTabs.tsx
+++ src/components/RestaurantTabs.tsx
@@ -16,16 +16,16 @@ const RestaurantTabs: React.FC<RestaurantTabsProps> = ({
   selectedRestaurant,
 }) => {
   return (
-    <div className='bg-gray-200 p-4 mb-4'>
-      <ul className='flex space-x-4 text-xs md:text-lg'>
-        {restaurants.map((restaurant) => (
+    <div className='bg-gray-200 mb-4 overflow-x-auto'>
+      <ul className='flex space-x-3 md:space-x-5 text-xs md:text-lg whitespace-nowrap p-4'>
+        {restaurants.map((restaurant, index) => (
           <li
             key={restaurant.restaurant_id}
             className={`cursor-pointer ${
               selectedRestaurant == restaurant.restaurant_id
                 ? 'font-bold'
                 : 'font-light'
-            }`}
+            } ${index === restaurants.length - 1 ? 'pr-4' : ''}`}
           >
             <Link href={`/restaurant/${restaurant.restaurant_id}`}>
               {restaurant.name}
