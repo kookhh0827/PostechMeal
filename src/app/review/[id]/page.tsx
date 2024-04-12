ecc7877ebe9314cb6332a873c6ab397e784d5d0e
kookhh0827 <kookhh0827@gmail.com>
2024-04-12 11:43:02 +0000

    fix: test revalidate 1 sec

--- src/app/review/[id]/page.tsx
+++ src/app/review/[id]/page.tsx
@@ -10,7 +10,7 @@ import ReviewSection from '@/components/ReviewSection';
 import StarRating from '@/components/StarRating';
 
 export const dynamic = 'force-static';
-export const revalidate = 30;
+export const revalidate = 1;
 
 const fetchOrderItem = async (orderitem_id: number) => {
   const supabase = createClient();
